3b490ca3c01248f043a99e8e6dcdf3aa0dd75606
Ahmed_Shawki_Dev <ahmedshawki.dev@gmail.com>
2026-07-02 11:40:30 +0300

    Migrations

---
+++ backend/Migrations/20260702083953_adddataannotation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddataannotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalProjectGrade",
+                table: "Projects",
+                type: "decimal(5,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalProjectGrade",
+                table: "Projects");
+        }
+    }
+}
